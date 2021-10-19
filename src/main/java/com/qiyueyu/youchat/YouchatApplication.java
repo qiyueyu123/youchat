95c7ecf5d2e0587c47c8485783324a02b19d875c
qiyueyu <1430547687@qq.com>
2021-10-19 10:32:55 +0800

    创建项目

---
+++ src/main/java/com/qiyueyu/youchat/YouchatApplication.java
@@ -0,0 +1,13 @@
+package com.qiyueyu.youchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiyueyu/youchat/YouchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiyueyu.youchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
